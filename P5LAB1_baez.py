82ad2b7caed4f3f4e07da035ad484b915f06be62
Baezmils <144837199+Baezmils@users.noreply.github.com>
2023-10-31 14:58:09 -0400

    Add files via upload

--- P5LAB1_baez.py
+++ P5LAB1_baez.py
@@ -5,6 +5,7 @@
 # Feel free to fork this REPL and make changes.
 
 # first function: main_menu().
+import time
 
 def main_menu():
     print("Main Menu")
@@ -59,31 +60,40 @@ def duel():
   yugis_turn()
   
 def branded_despia():
-  print("your the challenger so you are going first!")
+  print("youre the challenger so you are going first!")
   starting_hand()
 
 def starting_hand():
+  print()
   print("your starting hand is: ")
+  print()
   print("aluber of despia, fallen of albaz, maxx C, branded fusion, and PSY-framegear gamma")
+  print()
   print("what do you do?")
   print("1. summon aluber of despia")
   print("2. summon fallen of albaz")
-  print("3. activate maxx C")  
+  print("3. activate maxx c")  
   print("4. activate branded fusion")
   print("5. you cannot do anything with PSY-framegear gamma unless responding to a monster effect")
   choice = input()
-  summon = input()
+  #summon = input()
   your_turn = True
   while your_turn:
     if choice == "summon aluber of despia": 
       summon = input("summon face up or in face down defense position?")
       if summon == "summon face up":
+        print()
         print("you summon aluber of despia in face up attack position!")
+        print()
+        time.sleep(1)
         print("aluber activates its effect to add a despia card from your deck!")
+        print()
+        time.sleep(1)
         print("yugi activates infinite impermanence to negate aluber of despia!")
         your_turn = False
       elif summon == "summon face down":
         print("you summon aluber of despia in face down defence position and pass the turn!")
+        time.sleep(1)
         your_turn = False
       
     
@@ -91,19 +101,28 @@ def starting_hand():
       summon = input("summon face up or in face down defense position?")
       if summon == "summon face up":
         print("you summon fallen of albaz in face up attack position!")
+        time.sleep(1)
         your_turn = False
       elif summon == "summon face down":
         print("you summon fallen of albaz in face down defence position and pass the turn!")
-        your_turn=False
+        time.sleep(1)
+        your_turn = False
+        your_turn = False
     
-    elif choice == "activate maxx C":
-     print("maxx C activates its effect!")
-     print("nothing happens...")
-     your_turn = False
+    elif choice == "activate maxx c":
+      print("maxx C activates its effect!")
+      print()
+      time.sleep(1)
+      print("nothing happens...")
+      time.sleep(1)
+      your_turn = False
     
     elif choice == "activate branded fusion":
       print("you activate branded fusion!")
+      time.sleep(1)
+      print()
       print("yugi activates ash blossom and joyous spring, negating branded fusion!")
+      time.sleep(1)
       your_turn = False
     
     else:
@@ -113,19 +132,24 @@ def starting_hand():
 
 def yugis_turn():
   print()
-  print("its now yugis turn!")
+  print("*" * 10, "its now yugis turn!", "*" * 10)
+  time.sleep(2)
   print()
   print("yugi special summons SSGSS GOKU from his deck allowing him to draw his entire deck!")
+  time.sleep(2)
   print()
   print("yugi drew all five pieces of exodia!")
+  time.sleep(2)
   print()
   print("EXODIA OBLITERATE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+  time.sleep(2)
   print()
   choice_back_door()
 # finally, we have main -- which we use to start the program 
 def main():
     print("M5LAB1 - Choose Your Own Adventure")
     main_menu()
+    print()
     print("Thanks for playing!")
 
 #start the program
